55f1b7fa1e0d543800c70a4e144353662985a106
Cynthia Uche <cynthianuchepro@gmail.com>
2022-08-18 07:58:01 -0700

    Add header file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header files */
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/*Global Variables*/
+
+extern char **environ;
+
+#endif
