69ae076fc9295ea4e2a181074e8b93a6ae807768
Cynthia Uche <cynthianuchepro@gmail.com>
2022-09-06 06:40:25 -0700

    Update header file

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 /* Header files */
 
 #include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,5 +14,13 @@
 /*Global Variables*/
 
 extern char **environ;
+extern char *newline;
+
+
+/*Prototypes*/
+
+int execute(char **args);
+char *read_line(void);
+char **tokenize(char *buffer);
 
 #endif
