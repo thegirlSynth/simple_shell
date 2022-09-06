afc583d796d8177db438080bd5a29d50be9e37e9
Cynthia Uche <cynthianuchepro@gmail.com>
2022-09-06 06:36:22 -0700

    Fix exit error for child process

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -1,54 +1,98 @@
 #include "main.h"
 
 int execute(char **args);
+char *read_line(void);
+char **tokenize(char *buffer);
+
 /**
  * main - super simple unix shell. Version 1.0.
  * Return: 0 on success.
  */
 
-int main(void)
+int main(/*int argc , char **argv*/void)
 {
-	char *buffer = NULL, *prompt = "$ ", *newline = "\n";
-	size_t len = 0, ret;
-	ssize_t nread;
-	char *args[] = {NULL, NULL};
+	char *buffer, *prompt = "$ ";
+	char **args;
+	int status;
 
-	while (1)
-	{
+
+	do {
 		if (isatty(STDIN_FILENO))
-			write(1, prompt, 2);
+			write(1, prompt, 2); 
+		buffer = read_line();
+		args = tokenize(buffer);
+		status = execute(args);
 
-		nread = getline(&buffer, &len, stdin);
-		buffer[nread - 1] = '\0';
-		args[0] = buffer;
+		free(buffer);
+		free(args);
 
+	} while (status);
+
+	return (EXIT_SUCCESS);
+
+}
+
+
+/**
+ * read_line - gets input from the terminal. 
+ * Return: the arrray of characters read.
+ */
+
+char *read_line(void)
+{
+	char *buffer = NULL, *newline = "\n";
+	size_t len = 0;
+	ssize_t nread;
+
+	nread = getline(&buffer, &len, stdin);
+	buffer[nread - 1] = '\0';
+	if (nread == -1)
+	{
 		if (nread == EOF)
 		{
 			if (isatty(STDIN_FILENO))
-				write(1, newline, 1);
-			free(buffer);
-			return (0);
+				write(1, newline, 2);
+			exit(EXIT_SUCCESS);
 		}
 
-		ret = execute(args);
-		if (ret == 1)
+		else
 		{
-			free(buffer);
-			return (1);
+			perror("readline");
+			exit(EXIT_FAILURE);
 		}
 	}
 
-	free(buffer);
+	
+	return (buffer);
+}
 
-	return (0);
+/**
+ * tokenize - tokenize arguments
+ */
+ 
+char **tokenize(char *buffer)
+{
+	int index = 0;
+	char **args, *token;
 
+	args = malloc(sizeof(char *) * strlen(buffer));
+	if (args == NULL)
+		return (NULL);
+
+	token = strtok(buffer, " ");
+
+	while (token != NULL)
+	{
+		args[index] = strdup(token);
+		index++;
+		token = strtok(NULL, " ");
 	}
 
-
-	free(buffer);
-	return (0);
-
+	return (args);
 }
+
+
+
 
 /**
  * execute - execute commands
@@ -60,11 +104,12 @@ int execute(char **args)
 {
 	pid_t child_pid;
 
+
 	child_pid = fork();
 	if (child_pid == -1)
 	{
 		perror("Error:");
-		return (1);
+		return (-1);
 	}
 
 	if (child_pid == 0)
@@ -72,11 +117,11 @@ int execute(char **args)
 		if (execve(args[0], args, environ) == -1)
 		{
 			perror("./hsh");
-			return (1);
+			exit (EXIT_FAILURE);
 		}
 	}
 
 	wait(NULL);
 
-	return (0);
+	return (1);
 }
